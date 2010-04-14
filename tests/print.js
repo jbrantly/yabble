47afa94936ad3fc0a7973dd5aae0aabfde6c55a4
James Brantly <james@jbrantly.com>
2010-04-13 20:13:43 -0400

    Using QUnit for unit tests

--- tests/print.js
+++ tests/print.js
@@ -1,33 +1,3 @@
-(function() {
-	
-	var messages = [];
-	var outputDiv;
-	
-	window.onload = function() {
-		//console.log('onload', messages.length);
-		var body = document.getElementsByTagName('body')[0];
-		outputDiv = document.createElement('div');
-		body.appendChild(outputDiv);
-		
-		for (var i = 0, n = messages.length; i<n; i++) {
-			print(messages[i]);
-		}
-	};
-	
-	
-	window.print = function() {
-		if (outputDiv) {
-			var div = document.createElement('div');
-			for (var i = 0, n = arguments.length; i<n; i++) {
-				var span = document.createElement('span');
-				span.appendChild(document.createTextNode(arguments[i]));
-				div.appendChild(span);
-			}
-			outputDiv.appendChild(div);
-		}
-		else {
-			messages.push(Array.prototype.slice(arguments, 0));
-		}
-	}
-	
-})();
+window.print = function(message, type) {
+	ok(type == 'info' || type == 'pass', message);
+}
